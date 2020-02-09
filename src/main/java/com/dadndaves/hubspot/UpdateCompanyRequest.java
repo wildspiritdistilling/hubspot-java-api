abac82f1d5e3f64cedccdf3ab74bb2d2c89d9649
James Dumay <james@dadndavesbrewing.com>
2020-02-09 13:14:17 +1100

    add fromString add toString

--- src/main/java/com/dadndaves/hubspot/UpdateCompanyRequest.java
+++ src/main/java/com/dadndaves/hubspot/UpdateCompanyRequest.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
+import java.util.StringJoiner;
 
 public class UpdateCompanyRequest {
     public final List<Property> properties;
@@ -24,7 +25,12 @@ public class UpdateCompanyRequest {
             this.value = value;
         }
 
+        public static Property fromString(String name, String value) {
+            return new Property(name, value);
+        }
+
         public static Property fromDate(String name, Date value) {
+            // Need to convert to midnight on this day or hubspot has a tantrum
             long time = value.getTime();
             value = new Date(time - time % (24 * 60 * 60 * 1000));
             return new Property(name, Long.toString(value.getTime()));
@@ -32,6 +38,23 @@ public class UpdateCompanyRequest {
 
         public static Property fromDouble(String name, Double value) {
             return new Property(name, Double.toString(value));
+        }
+
+        public static Property fromOptions(String name, List<String> options) {
+            StringBuilder sb = new StringBuilder();
+            options.forEach(s -> {
+                sb.append(s);
+                sb.append(';');
+            });
+            return new Property(name, sb.toString());
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", Property.class.getSimpleName() + "[", "]")
+                    .add("name='" + name + "'")
+                    .add("value='" + value + "'")
+                    .toString();
         }
     }
 }
