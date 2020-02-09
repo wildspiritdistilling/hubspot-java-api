f6c83fdacad595bd9f1bf28dfe17aa2df27a828a
James Dumay <james@dadndavesbrewing.com>
2020-02-09 13:14:31 +1100

    update to use test hubspot secret

--- src/test/java/com/dadndaves/hubspot/HubSpotTest.java
+++ src/test/java/com/dadndaves/hubspot/HubSpotTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class HubSpotTest {
     @Test
     public void testIterateThroughCompaniesWithoutError() throws Exception {
-        HubSpot hubSpot = new HubSpot("c178c7a6-6d29-469c-bc7a-994f8fd8e592");
+        HubSpot hubSpot = new HubSpot("c5ce8ef3-cd80-41aa-b7ef-7dc8cdc0dd43");
         hubSpot.getCompanies().forEach(company -> {
             Assert.assertNotNull(company);
             System.out.println(company.properties.get("name").value);
